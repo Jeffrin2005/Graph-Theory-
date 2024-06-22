b28000ff2fa5e1190d1477c2f9da1dc560111d64
Jeffrin Jojo <135723871+Jeffrin2005@users.noreply.github.com>
2024-06-22 12:46:12 +0530

    Create Find the Town Judge.cpp

---
+++ Find the Town Judge.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& v) {
+        vector<int> ind(n + 1, 0);
+        int n1 = v.size();
+        for(int i=0; i<n1; i++){
+            ind[v[i][0]]--;
+            ind[v[i][1]]++;
+        }
+        for(int i=1; i<=n; i++){
+            if(ind[i]== (n-1)){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
