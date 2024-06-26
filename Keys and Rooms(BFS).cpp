a673b51b267916ce5a1e5bc76c7f3f2f572add04
Jeffrin Jojo <135723871+Jeffrin2005@users.noreply.github.com>
2024-06-26 22:50:33 +0530

    Update Keys and Rooms(BFS).cpp

--- Keys and Rooms(BFS).cpp
+++ Keys and Rooms(BFS).cpp
@@ -1,31 +1,34 @@
 class Solution {
-public:
-    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+private:
+    bool bfs(vector<vector<int>>& rooms, vector<int>& visited) {
         int n = rooms.size();
-        vector<int>visited(n, 0);
-        queue<int>q;
-        q.push({0});
+        queue<int> q;
+        q.push(0);
         visited[0] = 1;
-        for(int i=0; i<rooms[0].size(); i++){
-            q.push(rooms[0][i]);
-        }
-       while(!q.empty()){
+
+        while (!q.empty()) {
             int t = q.front();
             q.pop();
-            if(!visited[t]){
-                visited[t] = 1;
-                for(int i=0; i<rooms[t].size(); i++){
-                    q.push(rooms[t][i]);
+            for (int key : rooms[t]) {
+                if (!visited[key]) {
+                    visited[key] = 1;
+                    q.push(key);
                 }
             }
+        }
 
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                return false;
             }
-            for(int i=0; i<n;i++){
-                if(!visited[i]){
-                    return false;
-                }
-            }
-            return true; 
-       }
-    
+        }
+        return true;
+    }
+public:
+
+        bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        int n = rooms.size();
+        vector<int> visited(n, 0);
+        return bfs(rooms, visited);
+    }
 };
