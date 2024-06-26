710747d1e9b6775cf07ad04b0213cff266d597b5
Jeffrin Jojo <135723871+Jeffrin2005@users.noreply.github.com>
2024-06-26 22:24:58 +0530

    Update Find Eventual Safe States(BFS).cpp

--- Find Eventual Safe States(BFS).cpp
+++ Find Eventual Safe States(BFS).cpp
@@ -1,40 +1,43 @@
-
 class Solution {
-public:
-    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int V = graph.size();
-        vector<vector<int>> adjRev(V);
-        vector<int> indegree(V, 0);
-        
-        for (int i = 0; i < V; i++) {
-            for (auto it : graph[i]) {
-                adjRev[it].push_back(i);
-                indegree[i]++;
-            }
-        }
-        
-        queue<int> q;
-        vector<int> safeNodes;
-        
-        for (int i = 0; i < V; i++) {
-            if (indegree[i] == 0) {
-                q.push(i);
-            }
-        }
-        
-        while (!q.empty()) {
+private:
+    void bfs(vector<vector<int>>&adj, vector<int>&indegree, queue<int>&q;vector<int>&safenode, vector<int>&visited){
+        while(!q.empty()){
             int node = q.front();
             q.pop();
-            safeNodes.push_back(node);
-            for (auto it : adjRev[node]) {
-                indegree[it]--;
-                if (indegree[it] == 0) {
-                    q.push(it);
+            safenode.push_back(node);
+            for(auto&newnode : adj[node]){
+                indegree[newnode]--;
+                if(indegree[newnode]==0 and !visited[newnode]){
+                    visited[newnode] = 1;
+                    q.push(newnode);
                 }
             }
         }
+    }
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int V = graph.size();
+        vector<vector<int>>adj(V);
+        vector<int>indegree(V, 0);
+        vector<int>visited(V, 0);
+        for(int i=0; i<V; i++){
+            for(auto&x : graph[i]){
+                adj[x].push_back(i);
+                indegree[i]++;
+            }
+        }
+        queue<int>q;
+        vector<int>safenode;
+        for(int i=0; i<V; i++){
+            if(indegree[i] == 0){
+                q.push(i);
+                visited[i] = 1; 
+            }
+        }
+        bfs(adj, indegree,q, safenode,visited);
+        sort(safenode.begin(),safenode.end());
+        return safenode;
+
         
-        sort(safeNodes.begin(), safeNodes.end());
-        return safeNodes;
     }
 };
