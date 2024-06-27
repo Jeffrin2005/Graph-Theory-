49f512d0043242f1bfe978d6173f4ad762fd9e9a
Jeffrin Jojo <135723871+Jeffrin2005@users.noreply.github.com>
2024-06-28 05:21:27 +0530

    Update Maximal Network Rank(BFS).cpp

--- Maximal Network Rank(BFS).cpp
+++ Maximal Network Rank(BFS).cpp
@@ -1,37 +1,43 @@
+
+
 class Solution {
-public:
-    int maximalNetworkRank(int n, vector<vector<int>>& roads) {
-            vector<int>adj[n];
-            for(int i=0; i<roads.size(); i++){
-                adj[roads[i][0]].push_back(roads[i][1]);
-                adj[roads[i][1]].push_back(roads[i][0]);
-            }
-            queue<int>q;
-            int maxi = 0;
-            vector<int>visited(n , 0);
-            for(int i=0;i< n;i++){
-                q.push(i);
-                visited[i] = 1;
-                while(!q.empty()){
-                    int current = q.front();
-                    q.pop();
-                    for(int j=0; j<n; j++){
-                        if(i != j){
-                            int val = adj[i].size() + adj[j].size();
-                            if(find(adj[i].begin(), adj[i].end(), j) != adj[i].end()){
-                                val--;
-                            }
-                            maxi = max(maxi, val);
-                        }
-                    }
-                    for(auto&newnode : adj[current]){
-                        if(!visited[newnode]){
-                            visited[newnode]  = 1;
-                            q.push(newnode);
-                        }
-                    }
+private:
+    void bfs(int start, vector<bool>& visited, vector<vector<int>>& adj) {
+        queue<int> q;
+        q.push(start);
+        visited[start] = true;
+        while (!q.empty()) {
+            int current = q.front();
+            q.pop();
+            for (auto& neighbor : adj[current]) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    q.push(neighbor);
                 }
             }
-            return maxi; 
+        }
+    }
+
+public:
+    int maximalNetworkRank(int n, vector<vector<int>>& roads) {
+        vector<vector<int>> adj(n);
+        for (int i = 0; i < roads.size(); i++) {
+            adj[roads[i][0]].push_back(roads[i][1]);
+            adj[roads[i][1]].push_back(roads[i][0]);
+        }
+
+        int maxi = 0;
+        vector<int> visited(n, 0);
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int val = adj[i].size() + adj[j].size();
+                if (find(adj[i].begin(), adj[i].end(), j) != adj[i].end()) {
+                    val--; 
+                }
+                maxi = max(maxi, val);
+            }
+        }
+
+        return maxi;
     }
 };
