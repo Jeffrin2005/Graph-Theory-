6dbbc01488b5822bbcb0b913b9fc30f5be4a5235
Jeffrin Jojo <135723871+Jeffrin2005@users.noreply.github.com>
2024-06-28 21:12:56 +0530

    Create Evaluate Division(BFS).cpp

---
+++ Evaluate Division(BFS).cpp
@@ -0,0 +1,45 @@
+
+class Solution {
+public:
+    map<string, map<string, double>> graph;
+    void build_graph(vector<vector<string>>& equations, vector<double>& values) {
+        for (int i = 0; i < equations.size(); i++) {
+            double k = values[i];
+            graph[equations[i][0]][equations[i][1]] = k;
+            graph[equations[i][1]][equations[i][0]] = 1.0 / k;
+        }
+    }
+    double bfs(string source, string destination) {
+        if (graph.find(source) == graph.end() || graph.find(destination) == graph.end()) return -1.0;
+        if (source == destination) return 1.0;
+        queue<pair<string, double>> q;
+        q.push({source, 1.0});
+        map<string, double> visited; 
+        visited[source] = 1.0;
+
+        while (!q.empty()){
+            auto [current, current_product] = q.front();
+            q.pop();
+            if (current == destination) {
+                return current_product;
+            }
+            for (auto& neighbour : graph[current]) {
+                if (!visited.count(neighbour.first)) {
+                    visited[neighbour.first] = current_product * neighbour.second;
+                    q.push({neighbour.first, visited[neighbour.first]});
+                }
+            }
+        }
+        return -1.0; 
+    }
+
+    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+        build_graph(equations, values);
+        vector<double> result;
+        for (auto& q : queries) {
+            double temp = bfs(q[0], q[1]);
+            result.push_back(temp);
+        }
+        return result;
+    }
+};
