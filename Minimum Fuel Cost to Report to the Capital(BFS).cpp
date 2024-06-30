d69f625a035e6d808e2bba0ef62e45e14bce9373
Jeffrin Jojo <135723871+Jeffrin2005@users.noreply.github.com>
2024-06-30 10:45:51 +0530

    Update Minimum Fuel Cost to Report to the Capital(BFS).cpp

--- Minimum Fuel Cost to Report to the Capital(BFS).cpp
+++ Minimum Fuel Cost to Report to the Capital(BFS).cpp
@@ -1,22 +1,8 @@
 class Solution {
 public:
-    long long minimumFuelCost(vector<vector<int>>& roads, int seats) {
-        int n = roads.size() + 1;
-        if (n == 1) return 0;
-
-        vector<vector<int>> adj(n);
-        vector<int> visited(n, 0);
-        vector<int> degree(n, 0);
-        for (auto& edge : roads) {
-            adj[edge[0]].push_back(edge[1]);
-            adj[edge[1]].push_back(edge[0]);
-            degree[edge[0]]++;
-            degree[edge[1]]++;
-        }
-
-        vector<int> people(n, 1);
+    long long bfs(vector<vector<int>>& adj, vector<int>& visited, vector<int>& degree, vector<int>& people, int seats) {
         queue<int> q;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < adj.size(); i++) {
             if (degree[i] == 1 && i != 0) {  
                 q.push(i);
                 visited[i] = 1;
@@ -35,7 +21,6 @@ public:
 
                 for (auto& newnode : adj[node]) {
                     if (!visited[newnode]) {
-                        visited[newnode] = 1;
                         degree[newnode]--;
                         people[newnode] += people[node];
                         if (degree[newnode] == 1 && newnode != 0) {
@@ -47,5 +32,23 @@ public:
             }
         }
         return ans;
+    }
+
+    long long minimumFuelCost(vector<vector<int>>& roads, int seats) {
+        int n = roads.size() + 1;
+        if (n == 1) return 0;
+
+        vector<vector<int>> adj(n);
+        vector<int> visited(n, 0);
+        vector<int> degree(n, 0);
+        for (auto& edge : roads) {
+            adj[edge[0]].push_back(edge[1]);
+            adj[edge[1]].push_back(edge[0]);
+            degree[edge[0]]++;
+            degree[edge[1]]++;
+        }
+
+        vector<int> people(n, 1);
+        return bfs(adj, visited, degree, people, seats);
     }
 };
