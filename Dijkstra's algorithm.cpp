d37118fde57a896f5db77316e956cfedbb53ee39
Jeffrin Jojo <135723871+Jeffrin2005@users.noreply.github.com>
2024-07-02 17:34:34 +0530

    Create Dijkstra's algorithm.cpp

---
+++ Dijkstra's algorithm.cpp
@@ -0,0 +1,26 @@
+ int n = graph.size();
+    vector<int> dist(n, numeric_limits<int>::max());
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+    dist[src] = 0;
+    pq.push({0, src});
+
+    while (!pq.empty()) {
+        int u = pq.top().second;
+        int d = pq.top().first;
+        pq.pop();
+
+        // If the distance is not the current shortest, skip processing
+        if (d > dist[u]) continue;
+
+        for (auto& edge : graph[u]) {
+            int v = edge.first;
+            int weight = edge.second;
+
+            // Relaxation step
+            if (dist[u] + weight < dist[v]) {
+                dist[v] = dist[u] + weight;
+                pq.push({dist[v], v});
+            }
+        }
+    }
