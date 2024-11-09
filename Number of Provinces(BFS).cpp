a31a43b30c5d5b0fd87c9775eea18ad7c0ba3b6e
Jeffrin Jojo <135723871+Jeffrin2005@users.noreply.github.com>
2024-11-09 23:02:32 +0530

    Update Number of Provinces(BFS).cpp

--- Number of Provinces(BFS).cpp
+++ Number of Provinces(BFS).cpp
@@ -1,3 +1,36 @@
+class Solution {// USING DFS 
+private: 
+    void dfs(int node, vector<vector<int>>&adjLs,vector<int>&vis){
+        vis[node] = 1;
+        for(auto&x : adjLs[node]){
+            if(!vis[x]){
+                dfs(x,adjLs,vis);
+            }
+        }
+    }
+public:
+    int findCircleNum(vector<vector<int>>&adj) {
+        int V = adj.size();
+        vector<vector<int>>adjLs(V); 
+        for(int i = 0; i < V; i++){//  Iterates through each city i.
+            for(int j = 0; j < V; j++){ //  Iterates through each city j to check if it's directly connected to city i.
+                if(adj[i][j] == 1 and i != j) {
+                    adjLs[i].push_back(j); 
+                    adjLs[j].push_back(i); 
+                }
+            }
+        }
+        vector<int>vis(V, 0); 
+        int cnt = 0; 
+        for(int i = 0; i < V; i++){
+            if(!vis[i]){ 
+                cnt++;
+                dfs(i,adjLs,vis); 
+            }
+        }
+        return cnt; 
+    }       
+};          // USING BFS
 class Solution {
 private: 
     void bfs(int start, vector<vector<int>>& adjLs, vector<int>& vis) {
