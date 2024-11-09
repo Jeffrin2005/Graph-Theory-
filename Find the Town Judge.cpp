9612c3a99e06cc8575da2af0b1e5f5d30d47170e
Jeffrin Jojo <135723871+Jeffrin2005@users.noreply.github.com>
2024-11-09 08:56:43 +0530

    Update Find the Town Judge.cpp

--- Find the Town Judge.cpp
+++ Find the Town Judge.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution {// USING SINGLE VECTOR 
 public:
     int findJudge(int n, vector<vector<int>>& v) {
         vector<int> ind(n + 1, 0);
@@ -13,6 +13,24 @@ public:
             }
         }
 
+        return -1;
+    }
+}; // USE TWO VECTORS 
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& v) {
+        vector<int> ind(n + 1, 0);
+        vector<int> out(n + 1, 0);
+        int n1 = v.size();
+        for(int i = 0; i < n1; i++){
+            out[v[i][0]]++;
+            ind[v[i][1]]++;
+        }
+        for(int i = 1; i <= n; i++){
+            if(ind[i] == n - 1 && out[i] == 0){
+                return i;
+            }
+        }
         return -1;
     }
 };
