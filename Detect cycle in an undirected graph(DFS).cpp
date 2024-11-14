86432471dc5e4c563302bd267fe7ec912f0c2661
Jeffrin Jojo <135723871+Jeffrin2005@users.noreply.github.com>
2024-11-14 08:42:54 +0530

    Update Detect cycle in an undirected graph(DFS).cpp

--- Detect cycle in an undirected graph(DFS).cpp
+++ Detect cycle in an undirected graph(DFS).cpp
@@ -1,22 +1,31 @@
-  private: 
-    bool dfs(int node, int parent,vector<int>&vis, vector<int> adj[]){
-        vis[node] = 1; 
-        for(auto adjacentNode: adj[node]){
-            if(!vis[adjacentNode]){
-                if(dfs(adjacentNode, node, vis, adj) == true) 
-                    return true; 
+class Solution {
+    private:
+    // Helper DFS function to detect cycle.
+    bool dfs(int node, int parent, vector<vector<int>>& adj, vector<bool>& visited) {
+        visited[node] = true;
+        for(auto &neighbor : adj[node]) {
+            if(!visited[neighbor]) {
+                if(dfs(neighbor, node, adj, visited))
+                    return true;
             }
-            else if(adjacentNode != parent) return true; 
+            else if(visited[neighbor]==1 && neighbor != parent){
+                return true;
+            }
         }
-        return false; 
+        return false;
     }
   public:
-    bool isCycle(int V, vector<int> adj[]) {
-        vector<int>vis(V,0);
-       for(int i = 0;i<V;i++){
-           if(!vis[i]) {
-               if(dfs(i, -1, vis, adj) == true) return true; 
-           }
-       }
-       return false; 
+    // Function to detect cycle in an undirected graph.
+    bool isCycle(vector<vector<int>>& adj) {
+       int V = adj.size();
+        vector<bool> visited(V, false);
+        
+        for(int i = 0; i < V; ++i) {
+            if(!visited[i]) {
+                if(dfs(i, -1, adj, visited))
+                    return true;
+            }
+        }
+        return false;
     }
+};
